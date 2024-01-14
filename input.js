871f84ddc89a107d557a92fb7a0d5a8d243f2123
Your Name <you@example.com>
2024-01-14 18:14:29 +0400

    jsprohw3_1

---
+++ input.js
@@ -0,0 +1,25 @@
+"use strict";
+// Страница добавления отзыва о продукте.
+// Должна содержать форму с полем для ввода названия продукта и текстовое поле для текста отзыва.
+// Кнопка "Добавить отзыв", которая сохраняет отзыв о продукте в localstorage.
+// Необходимо реализовать проверку, оба поля должны быть заполнены, если это не так, необходимо выводить ошибку пользователю.
+
+const inputFieled = document.querySelector('.product__name');
+const addText = document.querySelector('.input__review');
+const addButton = document.querySelector('.button__btn');
+const massegError = document.querySelector('.masseg__error');
+
+addButton.addEventListener('click', () => {
+    const product = inputFieled.value.trim();
+    const review = addText.value.trim();
+    if (product !=='' && review !=='') {
+        let reviewList = JSON.parse(localStorage.getItem(product)) || [];
+        // if (reviewList === null) {
+        //     reviewList = []
+        //  }
+        reviewList.push(review);
+        localStorage.setItem(product, JSON.stringify(reviewList))
+    } else{
+        massegError.textContent = " Заполнены не все поля ввода!"
+    }
+});
